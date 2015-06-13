4cfb3940cbf2d48fc7b56f87f1dbfdeacc18f442
Babis Karypidis <xabikos@hotmail.com>
2015-06-13 12:34:23 +0300

    Add Reactjs.net nuget package and render the first component server side

---
+++ JoggingTimeTracker.Web/Scripts/build/server.js
@@ -0,0 +1,2 @@
+﻿// Expose components globally so ReactJS.NET can use them
+var Components = require('expose?Components!./index');
---
+++ JoggingTimeTracker.Web/app/components/app.jsx
@@ -0,0 +1,28 @@
+﻿var ReactBootstrap = require('react-bootstrap');
+
+
+class App extends React.Component {
+  render() {
+	var Grid = ReactBootstrap.Grid;
+	var Row = ReactBootstrap.Row;
+	var Col = ReactBootstrap.Col;
+	
+	return (
+		<div>
+			
+			<Grid fluid={false} >
+				<Row>
+					<Col xs={12} md={9}>
+						First	
+					</Col>					
+					<Col xs={12} md={3}>
+						last
+					</Col>
+				</Row>
+			</Grid>
+		</div>
+	);
+  }
+}
+
+module.exports = App;
